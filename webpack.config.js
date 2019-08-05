b32e1f8b8774be38a14952631c09beee74248988
Alberto Colella <alberto.colella@gmail.com>
2019-08-05 10:40:13 +0200

    npm modules updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const extractCSS = new ExtractTextPlugin('[name].css');
 
 const config = {
+  mode: "development",
   //context: path.resolve(__dirname, 'src'),
   entry: "./src/sass/style.scss",
   output: {
@@ -13,16 +13,37 @@ const config = {
   },
   module: {
     rules: [
-      {
+     /* {
         test: /\.scss$/,
         loader: extractCSS.extract(['css-loader','sass-loader']),
         exclude: [path.resolve('node_modules')]
+      },*/
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+    //          publicPath: '../',
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
       },
-
-    ]
+    ],
   },
   plugins: [
-    extractCSS,
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
     new CopyWebpackPlugin([
         { from: 'src/index.html' },
         { from: 'src/assets/favicon.ico' },
